c1491b291a6f805d0bf375ffc2b3a3d0074da9eb
yuxnzs <yuchengliu0429@gmail.com>
2024-08-13 03:17:28 +0800

    Remove unnecessary sorting code

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ const { TeaGarden } = require("./db");
 const app = express();
 app.use(bodyParser.json());
 
-// 從資料庫查詢返回 JSON 資料，並對每個 document 的 teaData 按日期由遠到近排序，再對所有 document 按最早的 teaData 日期排序
+// 從資料庫查詢返回 JSON 資料，並將所有 document 按最早的 teaData 日期排序
 app.get("/tea/:id", (req, res) => {
   const teaGardenID = req.params.id;
   TeaGarden.find({ teaGardenID: parseInt(teaGardenID) })
     .then((documents) => {
-      documents.forEach((document) => {
-        // 對每個 document 的 teaData 進行排序
-        document.teaData.sort((a, b) => new Date(a.date) - new Date(b.date));
-      });
       // 對所有 document 按最早的 teaData 日期進行排序
       documents.sort(
         (a, b) => new Date(a.teaData[0].date) - new Date(b.teaData[0].date)
